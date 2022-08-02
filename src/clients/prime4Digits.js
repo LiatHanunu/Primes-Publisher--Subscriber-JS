0b5cdac9c219c8cd29feaac763b10a6a4ebbe483
LiatHanunu <liat.hanunu01@gmail.com>
2022-08-02 12:35:39 +0300

    updating clients: prime1,prime4Digits and mersenne

---
+++ src/clients/prime4Digits.js
@@ -0,0 +1,9 @@
+//this client gets a prime number and if the number is 4 digits, it print the subtraction of it from 10,000 (like if prime number is 9973, then print 27)
+
+const prime4Digits = (num)=>{
+    
+    if(Math.floor(num/10000)==0){
+        console.log(10000-num)
+    }
+}
+export default prime4Digits
